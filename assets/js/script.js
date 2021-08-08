f694b8fb20b85ecd81305cc7c548e2f4e233e3c4
gervyg <gervygonzalez@gmail.com>
2021-08-08 18:56:45 -0400

    quinto viajes chile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+//Para smooth scrolling navbar
+$(function(){
+
+    $("a").click(function(event){
+        if (this.hash !=="") {
+            event.preventDefault();    
+            var gato = this.hash;
+            $("html,body").animate({ scollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash = gato;
+            });   
+        }    
+    }); 
+});
+//Para mostrar tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
-
