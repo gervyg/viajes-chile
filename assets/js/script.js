7ceaef95d48612765dcdfcba0d77496860ba2ec2
gervyg <gervygonzalez@gmail.com>
2021-08-08 18:04:43 -0400

    cuarto viajes chile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
-/*$(function(){
-
-$("a").click(function(event){
-    if (this.hash !=="") {
-        event.defaultPrevented();
-
-        var gato = this.hash;
-        $("html,body").animate({ scolltop: $(gato.offset().top
-        }, 800,funtion(){
-            window.location.hash = gato;
-        });
-    
-    }
-
-});
-
-});*/
